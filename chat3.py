3891678eb19e8278d4d971caf1f3bce0375a80d9
Matt Chien <komm5233@hotmail.com>
2022-04-03 16:14:44 +0800

    add chat3.py

---
+++ chat3.py
@@ -0,0 +1,11 @@
+lines = []
+with open('line_Emma2.txt', 'r', encoding='utf-8') as f:
+	for line in f:
+		lines.append(line.strip())
+
+for line in lines:
+	s = line.split(' ')
+	time = s[0][:5]
+	name = s[0][5:]
+	print(name)
+
